20458a2f50f2cc790f3abeb9aa3c9a3e8d30b84a
Ellaiza Jean Curay Balatero <ejbalatero05@gmail.com>
2025-02-12 16:42:37 +0800

    Added Java Application

---
+++ 1T13A_BALATERO/IT13A_BALATERO/src/Act11_Arithmetic_Operators.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ACER
+ */
+public class Act11_Arithmetic_Operators {
+    public static void main(String[] args){
+        int firstnumber= 125;
+        int secondnumber= 24;
+        
+        int addition= firstnumber + secondnumber;
+        int subtraction= firstnumber - secondnumber;
+        int multiply= firstnumber * secondnumber;
+        int divide= firstnumber / secondnumber;
+        int module= firstnumber % secondnumber;
+        
+        System.out.println("125 + 24 = " + addition);
+        System.out.println("125 - 24 = " + subtraction);
+        System.out.println("125 x 24 = " + multiply);
+        System.out.println("125 / 24 = " + divide);
+        System.out.println("125 mod 24 + " + module);
+        
+       
+        
+        
+        
+        
+    }
+    
+}
