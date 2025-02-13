639be9f6c8cf411db643067743cc0b162590bd0f
Ellaiza Jean Curay Balatero <ejbalatero05@gmail.com>
2025-02-13 19:59:37 +0800

    ADDED Lab_act1_Arithmetics

--- 1T13A_BALATERO/Balatero_IT13A_Activities/src/PRELIM/Lab_Act1_Arithmetics.java
+++ 1T13A_BALATERO/Balatero_IT13A_Activities/src/PRELIM/Lab_Act1_Arithmetics.java
@@ -10,4 +10,21 @@ package PRELIM;
  */
 public class Lab_Act1_Arithmetics {
     
+    public static void main(String[] args){
+        int firstnumber= 10;
+        int secondnumber= 4;
+        int thirdnumber= 6;
+        
+        int addition= firstnumber * secondnumber + thirdnumber;
+        int subtraction= (firstnumber - secondnumber) % thirdnumber;
+        int multiply= (firstnumber + secondnumber + thirdnumber) / 3;
+        int division= firstnumber * thirdnumber - (secondnumber * secondnumber) ;
+        System.out.println("10 * 4 + 6 = " + addition);
+        System.out.println("10 - 4) % 6 = " + subtraction);
+        System.out.println("10 + 4 + 6 / 3 = " + multiply);
+        System.out.println("10 * 6 - 4 * 2 = "  + division);
+    }
+        
+   
+    
 }
