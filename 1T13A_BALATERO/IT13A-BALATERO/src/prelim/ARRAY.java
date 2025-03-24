ba66a75db2cba875751f40b765e2ba6a1b55ea74
Ellaiza Jean Curay Balatero <ejbalatero05@gmail.com>
2025-03-25 07:54:19 +0800

    added array

---
+++ 1T13A_BALATERO/IT13A-BALATERO/src/prelim/ARRAY.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MIDTERM;
+
+/**
+ *
+ * @author User
+ */
+public class ARRAY {
+    public static void main(String[] args){
+//        char [] letters = {'A', 'B', 'C', 'D', 'E','F', 'G'};
+//        
+//        System.out.println(letters[0]+ "," +letters [1] + "," +letters [2] + "," +letters [3] + "," + letters [4] + "," + letters [5] + "," + letters[6]);
+//        
+            char[] letters = {'A', 'B','C','D','E','F','G'};
+
+                 for (char t=0; t<letters.length; t++){
+                     System.out.print(letters[t]+ " , ");
+                 }
+                     System.out.println();
+    }
+    } 
+    
+            
+    
+
+
